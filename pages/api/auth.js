f26f20b543a60b748458189f7a7f0c4c7e1dc2f0
BrightTheBackpack <78445409+BrightTheBackpack@users.noreply.github.com>
2024-10-07 22:47:09 +0000

    add console thing

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -2,6 +2,7 @@
 // auth.js
 export default (req, res) => {
     const clientId = process.env.client_id; // Replace with your actual client_id
+    console.log(clientId)
     const redirectUri = "https://blot-review-thing.vercel.app/api/callback"; // Replace with your redirect URI
     const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}`;
     res.redirect(githubAuthUrl);
